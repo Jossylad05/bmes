3c8a9b9e1f80f42171a4d02ccd9413ab1483df62
Jossylad05 <64157929+Jossylad05@users.noreply.github.com>
2020-04-24 14:34:01 +0100

    Add project files.

---
+++ bmesproject/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\jossy\Documents\Dev\Fullstack Django and Python Bootcamp With Real Life Projects Ebenezer Ogbu\bmes\bmesproject\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
